945861ee0e2e3e4ff74aac9ee66f79cf8d910a52
cihan <bencakirim61@gmail.com>
2019-07-05 16:41:09 +0300

    Controller add

--- app/Http/Controllers/AnasayfaController.php
+++ app/Http/Controllers/AnasayfaController.php
@@ -7,6 +7,21 @@ use Illuminate\Http\Request;
 class AnasayfaController extends Controller
 {
     public function index(){
-        return view('anasayfa');
+
+        $isim = 'cihan';
+        $soyisim = 'Çakır';
+        $isimler = ['Can','Murat','Nazlı','Nesliha'];
+        $kullanicilar = [
+            ['id'=> 1, 'kullanici_adi'=> 'Can'],
+            ['id'=> 2, 'kullanici_adi'=> 'Murat'],
+            ['id'=> 3, 'kullanici_adi'=> 'Nazlı'],
+            ['id'=> 4, 'kullanici_adi'=> 'Nesliha'],
+            ['id'=> 5, 'kullanici_adi'=> 'memo'],
+            ['id'=> 6, 'kullanici_adi'=> 'Kero']
+
+        ];
+        //return view('anasayfa',['isim'=>'Cihan', 'soyisim'=>'Çakır']);
+        return view('anasayfa', compact('isim','soyisim','isimler','kullanicilar'));
+       // return view('anasayfa')->with(['isim'=>$isim, 'soyisim'=>$soyisim]);
     }
 }
